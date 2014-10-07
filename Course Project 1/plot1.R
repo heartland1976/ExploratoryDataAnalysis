978f7dca0797347fab1f02d92f4aa6e0606e2c3b
System Administrator <root@Jeroens-MacBook-Pro.local>
2014-10-07 23:44:17 +0200

    Committed first three plots

--- Course Project 1/plot1.R
+++ Course Project 1/plot1.R
@@ -1,3 +1,13 @@
+## Read the file into memory
+el <- read.csv("household_power_consumption.txt", sep=";", header=TRUE)
+
+## Subsetting the data
+el$ParsedDate <- as.Date(el$Date,"%d/%m/%Y")
+elParsed <- subset(el, el$ParsedDate == as.Date("2007-02-01") | el$ParsedDate == as.Date("2007-02-02"))
+
+as.numeric.factor <- function(x) {as.numeric(levels(x))[x]}
+elParsed$Global_active_power_num <- as.numeric.factor(elParsed$Global_active_power)
+
 hist(
   elParsed$Global_active_power_num, 
   main = "Global Active Power",
--- Course Project 1/plot3.R
+++ Course Project 1/plot3.R
@@ -1,3 +1,17 @@
+## Read the file into memory
+el <- read.csv("household_power_consumption.txt", sep=";", header=TRUE)
+
+## Subsetting the data
+el$ParsedDate <- as.Date(el$Date,"%d/%m/%Y")
+elParsed <- subset(el, el$ParsedDate == as.Date("2007-02-01") | el$ParsedDate == as.Date("2007-02-02"))
+
+elParsed$ParsedDateTime <- as.POSIXct(paste(elParsed$ParsedDate, elParsed$Time), format="%Y-%m-%d %H:%M:%S")
+
+## Plot3.png
+elParsed$Sub_metering_1_num <- as.numeric.factor(elParsed$Sub_metering_1)
+elParsed$Sub_metering_2_num <- as.numeric.factor(elParsed$Sub_metering_2)
+elParsed$Sub_metering_3_num <- as.numeric.factor(elParsed$Sub_metering_3)
+
 plot(
   elParsed$ParsedDateTime,
   elParsed$Sub_metering_1_num,
