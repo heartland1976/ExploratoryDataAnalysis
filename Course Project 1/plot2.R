0e3552fc3a27c6a21ae1eca98af12c82e028229f
System Administrator <root@Jeroens-MacBook-Pro.local>
2014-10-08 17:47:12 +0200

    Committed first three plots

--- Course Project 1/plot2.R
+++ Course Project 1/plot2.R
@@ -4,11 +4,10 @@ el <- read.csv("household_power_consumption.txt", sep=";", header=TRUE)
 ## Subsetting the data
 el$ParsedDate <- as.Date(el$Date,"%d/%m/%Y")
 elParsed <- subset(el, el$ParsedDate == as.Date("2007-02-01") | el$ParsedDate == as.Date("2007-02-02"))
+elParsed$ParsedDateTime <- as.POSIXct(paste(elParsed$ParsedDate, elParsed$Time), format="%Y-%m-%d %H:%M:%S")
 
 as.numeric.factor <- function(x) {as.numeric(levels(x))[x]}
 elParsed$Global_active_power_num <- as.numeric.factor(elParsed$Global_active_power)
-
-elParsed$ParsedDateTime <- as.POSIXct(paste(elParsed$ParsedDate, elParsed$Time), format="%Y-%m-%d %H:%M:%S")
 
 plot(
   elParsed$ParsedDateTime,
--- Course Project 1/plot3.R
+++ Course Project 1/plot3.R
@@ -4,13 +4,17 @@ el <- read.csv("household_power_consumption.txt", sep=";", header=TRUE)
 ## Subsetting the data
 el$ParsedDate <- as.Date(el$Date,"%d/%m/%Y")
 elParsed <- subset(el, el$ParsedDate == as.Date("2007-02-01") | el$ParsedDate == as.Date("2007-02-02"))
-
 elParsed$ParsedDateTime <- as.POSIXct(paste(elParsed$ParsedDate, elParsed$Time), format="%Y-%m-%d %H:%M:%S")
 
-## Plot3.png
+as.numeric.factor <- function(x) {as.numeric(levels(x))[x]}
+elParsed$Global_active_power_num <- as.numeric.factor(elParsed$Global_active_power)
+
 elParsed$Sub_metering_1_num <- as.numeric.factor(elParsed$Sub_metering_1)
 elParsed$Sub_metering_2_num <- as.numeric.factor(elParsed$Sub_metering_2)
-elParsed$Sub_metering_3_num <- as.numeric.factor(elParsed$Sub_metering_3)
+elParsed$Sub_metering_3_num <- as.numeric(elParsed$Sub_metering_3)
+
+## I am a bit color blind, I cannot really distinguish the colors well
+## Hope I did it well
 
 plot(
   elParsed$ParsedDateTime,
@@ -26,9 +30,10 @@ lines(
   elParsed$Sub_metering_2_num, 
   col = "red"
 )
+
 lines(
   elParsed$ParsedDateTime,
-  elParsed$Sub_metering_3_num, 
+  elParsed$Sub_metering_3, 
   col = "blue"
 )
 
